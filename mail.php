34046a74fd16b500e5ae8ad52e7a7289747242fc
muhammadusmansadiq <64164490+muhammadusmansadiq@users.noreply.github.com>
2022-12-07 19:49:41 +0500

    Add files via upload

--- mail.php
+++ mail.php
@@ -1,35 +1,29 @@
 <?php
+include "src/Smtp.php"
 
+$mail = new gclinux\Smtper();
 
-require_once "Mail.php";
+ $mail->setDebug(true); //do you want to output the debug information.是否输出调试信息
 
-$from = '<noreply@magiscriptor.com>';
-$to = '<usmansadiq721@gmail.com>';
-$subject = 'Hi from smtp!';
-$body = "Hi,\n\nHow are you?";
+ $mail->setServer("smtp.gmail.com", "noreply@magiscriptor.com", "M@steR12!@"); //Setting the SMTP server without SSL. 无SSL的SMTP服务器设置
 
-$headers = array(
-    'From' => $from,
-    'To' => $to,
-    'Subject' => $subject
-);
+$mail->setServer("smtp.gmail.com", "noreply@magiscriptor.com", "M@steR12!@", 465, true); //Seeting the SMTP server with SSL .SSL的SMTP服务器设置
 
-$smtp = Mail::factory('smtp', array(
-        'host' => 'ssl://smtp.gmail.com',
-        'port' => '465',
-        'auth' => true,
-        'username' => 'noreply@magiscriptor.com',
-        'password' => 'M@steR12!@'
-    ));
+ $mail->setFrom("noreply@magiscriptor.com"); //Email Sender name 发送者
 
-$mail = $smtp->send($to, $headers, $body);
+ $mail->setReceiver("usmansadiq@live.co.uk"); //Email reciver 接收者
+//  $mail->setReceiver("XXXXX2@local"); //Email reciver,multiple calls will add recivers.多次调用会累计添加接受者
+ 
+//  $mail->setCc("XXXX"); //Set CC .抄送
+// $mail->setBcc("XXXXX"); //Set CC,multiple calls will append.多次调用会添加.
 
-if (PEAR::isError($mail)) {
-    echo('<p>' . $mail->getMessage() . '</p>');
-} else {
-    echo('<p>Message successfully sent!</p>');
-}
+//  $mail->addAttachment("XXXX.png"); //Attachment 附件
 
+//  $mail->addAttachment("XXXX.csv"); // 添加附件，多个附件，调用多次 
+
+ $mail->setMail("from smtp ", "hello m seding from smtp"); //the titile and body 标题和内容
+
+$mail->send();
 
 
 ?>
